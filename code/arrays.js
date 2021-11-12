3b5ce47eb6b82797d097f5f90ba7ce46ae986666
César González <chesarglez429@gmail.com>
2021-11-12 08:44:34 -0600

    :sparkles: Finish first part of arrays

--- code/arrays.js
+++ code/arrays.js
@@ -3,6 +3,7 @@ console.log( arreglo[0] ) // "a"
 console.log( arreglo[1] ) // "b"
 
 // La siguiente instrucción no reasigna la variable arreglo,
-// sino el tercer elemento de arreglo
+// sino que cambia qué valor guarda el arreglo en el índice 2
+
 arreglo[2] = "mutación"
 console.log( arreglo ) // [ "a", "b", "mutación" ]
