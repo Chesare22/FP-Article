888d6c524486193067eb64dc3e72582bd481fdc2
César González <chesarglez429@gmail.com>
2021-11-13 19:50:39 -0600

    :fire: Remove modulus from the operators section

--- code/operators/arithmetic.js
+++ code/operators/arithmetic.js
@@ -4,5 +4,3 @@ console.log( "Tardes" + " ya :)" ) // "Tardes ya :)"
 console.log( 20 - 15 ) // 5
 console.log( 3 * 7 ) // 21
 console.log( 9 / 3 ) // 3
-console.log( 17 % 3 ) // 2
-console.log( 18 % 3 ) // 0
