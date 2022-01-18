8a275e1b12525561062e39f71070e90187b648b0
César González <chesarglez429@gmail.com>
2022-01-17 18:01:17 -0600

    :art: Format files

--- code/cicles/for.js
+++ code/cicles/for.js
@@ -1,5 +1,5 @@
 for ( let número = 0; número < 3; número = número + 1 ) {
-  console.log( número ) // 0, 1, 2  
+  console.log( número ) // 0, 1, 2
 }
 
 console.log( "Alcanzable :D" ) // "Alcanzable :D"
